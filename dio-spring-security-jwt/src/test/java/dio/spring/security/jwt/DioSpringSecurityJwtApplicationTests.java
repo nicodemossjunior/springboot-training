b2175199208c5097de4291ff7610b432b206c069
nicodemossjunior <nicodemossjunior@gmail.com>
2025-08-14 21:42:39 -0300

    project spring-security-jwt created

---
+++ dio-spring-security-jwt/src/test/java/dio/spring/security/jwt/DioSpringSecurityJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package dio.spring.security.jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = DioSpringSecurityJwtApplication.class)
+class DioSpringSecurityJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
